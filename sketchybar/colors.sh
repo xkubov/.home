48c16735118c49781aafd939140692358bfc8ff5
Peter Kubov <peter.kubov@skippay.cz>
2025-11-02 19:01:15 +0100

    Update config

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-# Color Palette
+# Color Palette (matched to Alacritty)
 export BLACK=0xff282828
 export WHITE=0xffdfdfe0
 export RED=0xffee5396
 export GREEN=0xff25be6a
-export BLUE=0xff8a9ff
+export BLUE=0xff78a9ff
 export YELLOW=0xff08bdba
 export ORANGE=0xfff5a97f
+export CYAN=0xff33b1ff
 export MAGENTA=0xffbe95ff
 export GREY=0xff939ab7
 export TRANSPARENT=0x00000000
 
+# Alacritty colors
+export BACKGROUND=0xff161616
+export FOREGROUND=0xfff2f4f8
+
 # General bar colors
-export BAR_COLOR=0x001e1e2e
-export BAR_BORDER_COLOR=0xff494d64 #0xa024273a
-export ICON_COLOR=$WHITE           # Color of all icons
-export LABEL_COLOR=$WHITE          # Color of all labels
-export BACKGROUND_1=$BLACK
+export BAR_COLOR=0x00000000
+export BAR_BORDER_COLOR=0xffdfdfe0 # White border
+export ICON_COLOR=$FOREGROUND       # Color of all icons
+export LABEL_COLOR=$FOREGROUND      # Color of all labels
+export BACKGROUND_1=$BACKGROUND
 export BACKGROUND_2=$TRANSPARENT
 
-export POPUP_BACKGROUND_COLOR=$BLACK
-export POPUP_BORDER_COLOR=$WHITE
+export POPUP_BACKGROUND_COLOR=$BACKGROUND
+export POPUP_BORDER_COLOR=$FOREGROUND
 
 export SHADOW_COLOR=$BLACK
--- sketchybar/plugins/disk.sh
+++ sketchybar/plugins/disk.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sketchybar -m --set usage.disk label="$(df -H | grep -E '^(/dev/disk3s5).' | awk '{ printf ("%s\n", $5) }')"
+sketchybar -m --set usage.disk label="$(df -H / | tail -1 | awk '{ printf ("%s\n", $5) }')"
